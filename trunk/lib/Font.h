4e801d1e0cd69a323a3bacccbebe77786a8ccb2e
matt.mets@gmail.com <matt.mets@gmail.com@a9283160-f6ed-11de-885d-7376d8f76b83>
2010-07-19 06:45:40 +0000

    Move fonts to progmem, to lower ram usage. (Issue 8) Fix mis-handling of last row (Issue 9)

--- trunk/lib/Font.h
+++ trunk/lib/Font.h
@@ -30,9 +30,9 @@
 namespace Font
 {
 
-extern uint8_t Draw(uint16_t letter,int x,int y,int set=1);
+extern uint8_t Draw(unsigned char letter,int x,int y,int set=1);
 
-extern uint8_t Draw90(uint16_t letter,int x,int y,int set=1);
+extern uint8_t Draw90(unsigned char letter,int x,int y,int set=1);
 
 }
 
